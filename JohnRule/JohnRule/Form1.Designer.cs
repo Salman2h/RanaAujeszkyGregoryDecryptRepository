5b84c7641cca2cdc771a23b42921f7b055b5afa2
Rashid Rana Salman <RRashid@schools.nyc.gov>
2017-11-30 13:50:58 -0500

    John Rule for Penn Testing

---
+++ JohnRule/JohnRule/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace JohnRule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ByttonGenerateRule = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ByttonGenerateRule
+            // 
+            this.ByttonGenerateRule.Location = new System.Drawing.Point(86, 148);
+            this.ByttonGenerateRule.Name = "ByttonGenerateRule";
+            this.ByttonGenerateRule.Size = new System.Drawing.Size(75, 23);
+            this.ByttonGenerateRule.TabIndex = 0;
+            this.ByttonGenerateRule.Text = "Generate Rule";
+            this.ByttonGenerateRule.UseVisualStyleBackColor = true;
+            this.ByttonGenerateRule.Click += new System.EventHandler(this.ButtonGenerateRule_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(86, 35);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.ByttonGenerateRule);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ByttonGenerateRule;
+        private System.Windows.Forms.TextBox textBoxResult;
+    }
+}
+
---
+++ JohnRule/JohnRule/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace JohnRule
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static List<string> LoadDictionary(string filePath)
+        {
+            List<string> dictionaryItems = new List<string>();
+            dictionaryItems = File.ReadAllLines(filePath).ToList();
+            return dictionaryItems;
+        }
+
+        private void BuildFirstHalf(string password)
+        {
+
+            StreamWriter file;
+
+            string temp = password;
+            int aOccurence = password.IndexOf("a");
+
+            string[] aArray;
+
+            aArray = password.Split('a');
+            int aCount = aArray.Length - 1;
+
+
+            string orginalPassword = password;
+            for (int i = 0; i < aCount; i++)
+            {
+                file = new StreamWriter(@"../../newwordlist.txt", true);
+                if (i == 0)
+                {
+                    //write to a file
+                    textBoxResult.Text = password;
+                    file.WriteLine(password);
+                    
+                    /* Added for single occurance
+                     * 
+                                        
+                     * */
+
+                    aArray = temp.Split('a');
+                    aCount = aArray.Length - 1;
+
+                    string singleOccurance = temp;
+
+                    for (int j = 0; j < aCount; j++)
+                    {
+
+                        int aTemp = singleOccurance.IndexOf("a");
+
+                        StringBuilder sbTemp = new StringBuilder(temp);
+                        sbTemp[aTemp] = '4';
+
+                        if (j > 0)
+                        {
+                            textBoxResult.Text = sbTemp.ToString();
+                            file.WriteLine(sbTemp.ToString());                            
+                        }
+
+                        StringBuilder sbSingleOccurance = new StringBuilder(singleOccurance);
+                        sbSingleOccurance[aTemp] = '4';
+                        singleOccurance = sbSingleOccurance.ToString();
+
+                    }
+
+
+
+                    // End single occurance
+
+                }
+
+                int aIndex = password.IndexOf("a");
+
+                StringBuilder sb = new StringBuilder(password);
+                sb[aIndex] = '4';
+                password = sb.ToString();
+
+                //write to a file
+                textBoxResult.Text = password;
+                file.WriteLine(password);
+                file.Close();
+            }
+        }
+
+        private void BuildSecondHalf(string password)
+        {
+            // teamst3rs
+            // te4mst3rs
+            StreamWriter file;
+
+            string temp = password;
+
+            string[] eArray;
+            eArray = password.Split('e');
+            int eCount = eArray.Length - 1;
+            string orginalPassword = password;
+
+            temp = orginalPassword;
+            for (int i = 0; i < eCount; i++)
+            {
+
+                int aIndex = password.IndexOf("e");
+
+                StringBuilder sb = new StringBuilder(password);
+                sb[aIndex] = '3';
+                password = sb.ToString();
+
+                int aOrginalIndex = orginalPassword.IndexOf("e");
+
+                StringBuilder sbOriginal = new StringBuilder(orginalPassword);
+                sbOriginal[aOrginalIndex] = '3';
+                orginalPassword = sbOriginal.ToString();
+
+                file = new StreamWriter(@"../../newwordlist.txt", true);
+
+                if (i == 0)
+                {
+                    eArray = temp.Split('e');
+                    eCount = eArray.Length - 1;
+
+                    string singleOccurance = temp;
+
+                    for (int j = 0; j < eCount; j++)
+                    {
+
+                        int aTemp = singleOccurance.IndexOf("e");
+
+                        StringBuilder sbTemp = new StringBuilder(temp);
+                        sbTemp[aTemp] = '3';
+
+                        if (j > 0)
+                        {
+                            textBoxResult.Text = sbTemp.ToString();
+                            file.WriteLine(sbTemp.ToString());
+                        }
+
+                        StringBuilder sbSingleOccurance = new StringBuilder(singleOccurance);
+                        sbSingleOccurance[aTemp] = '3';
+                        singleOccurance = sbSingleOccurance.ToString();
+
+                    }
+                }
+
+                //write to a file
+                textBoxResult.Text = password;
+                file.WriteLine(password);
+                textBoxResult.Text = orginalPassword;
+                file.WriteLine(orginalPassword);
+                file.Close();
+            }
+        }
+
+        private void ButtonGenerateRule_Click(object sender, EventArgs e)
+        {
+
+            List<string> plainTextList = LoadDictionary(@"../../wordlist.txt");
+
+            foreach (string plainText in plainTextList)
+            {
+                StreamWriter file;
+                string password = plainText;
+                string temp = password;
+                int aOccurence = password.IndexOf("a");
+
+                string[] aArray;
+
+                aArray = password.Split('a');
+                int aCount = aArray.Length - 1;
+
+
+                string orginalPassword = password;
+                for (int i = 0; i < aCount; i++)
+                {
+                    file = new StreamWriter(@"../../newwordlist.txt", true);
+                    if (i == 0)
+                    {
+                        aArray = temp.Split('a');
+                        aCount = aArray.Length - 1;
+
+                        string singleOccurance = temp;
+
+                        for (int j = 0; j < aCount; j++)
+                        {
+
+                            int aTemp = singleOccurance.IndexOf("a");
+
+                            StringBuilder sbTemp = new StringBuilder(temp);
+                            sbTemp[aTemp] = '4';
+
+                            if (j > 0)
+                            {
+                                textBoxResult.Text = sbTemp.ToString();
+                                file.WriteLine(sbTemp.ToString());
+                            }
+
+                            StringBuilder sbSingleOccurance = new StringBuilder(singleOccurance);
+                            sbSingleOccurance[aTemp] = '4';
+                            singleOccurance = sbSingleOccurance.ToString();
+                        }
+
+                        // End single occurance
+                    }
+
+                    int aIndex = password.IndexOf("a");
+
+                    StringBuilder sb = new StringBuilder(password);
+                    sb[aIndex] = '4';
+                    password = sb.ToString();
+
+                    //write to a file
+                    textBoxResult.Text = password;
+                    file.WriteLine(password);
+                    file.Close();
+                }
+
+                // teamst3rs
+                // te4mst3rs
+                string[] eArray;
+                eArray = password.Split('e');
+                int eCount = eArray.Length - 1;
+
+                temp = orginalPassword;
+                for (int i = 0; i < eCount; i++)
+                {
+                    int aIndex = password.IndexOf("e");
+
+                    StringBuilder sb = new StringBuilder(password);
+                    sb[aIndex] = '3';
+                    password = sb.ToString();
+
+                    int aOrginalIndex = orginalPassword.IndexOf("e");
+
+                    StringBuilder sbOriginal = new StringBuilder(orginalPassword);
+                    sbOriginal[aOrginalIndex] = '3';
+                    orginalPassword = sbOriginal.ToString();
+
+                    file = new StreamWriter(@"../../newwordlist.txt", true);
+
+                    if (i == 0)
+                    {
+                        eArray = temp.Split('e');
+                        eCount = eArray.Length - 1;
+
+                        string singleOccurance = temp;
+
+                        for (int j = 0; j < eCount; j++)
+                        {
+
+                            int aTemp = singleOccurance.IndexOf("e");
+
+                            StringBuilder sbTemp = new StringBuilder(temp);
+                            sbTemp[aTemp] = '3';
+
+                            if (j > 0)
+                            {
+                                textBoxResult.Text = sbTemp.ToString();
+                                file.WriteLine(sbTemp.ToString());
+                            }
+
+                            StringBuilder sbSingleOccurance = new StringBuilder(singleOccurance);
+                            sbSingleOccurance[aTemp] = '3';
+                            singleOccurance = sbSingleOccurance.ToString();
+                        }
+                    }
+
+                    //write to a file
+                    textBoxResult.Text = password;
+                    file.WriteLine(password);
+                    textBoxResult.Text = orginalPassword;
+                    file.WriteLine(orginalPassword);
+                    file.Close();
+                }
+                BuildFirstHalf(temp.Replace("e", "3"));
+                BuildSecondHalf(temp.Replace("a", "4"));
+            }
+            MessageBox.Show("Wordlist generated!");
+        }
+
+    }
+
+  
+}
